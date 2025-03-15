d5811b13468bc86849ab55824311380f150e69d6
vinicius marques <viniciusmc486@gmail.com>
2025-03-15 14:07:16 -0300

    Add POST method to Hotel resource for creating new hotel entries

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
@@ -24,9 +24,11 @@ hoteis = [
     }
 ]
 
+
 class Hoteis(Resource):
     def get(self):
         return hoteis
+
 
 class Hotel(Resource):
     def get(self, hotel_id):
@@ -34,6 +36,28 @@ class Hotel(Resource):
             if hotel['hotel_id'] == hotel_id:
                 return hotel
         return {'message': 'Hotel not found.'}, 404
-    
+
     def post(self, hotel_id):
-        pass
+
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return {'message': 'Hotel already exists'}, 409
+
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('name')
+        argumentos.add_argument('stars')
+        argumentos.add_argument('daily')
+        argumentos.add_argument('city')
+
+        dados = argumentos.parse_args()
+
+        new_hotel = {
+            'hotel_id': hotel_id,
+            'name': dados['name'],
+            'stars': dados['stars'],
+            'daily': dados['daily'],
+            'city': dados['city'],
+        }
+        hoteis.append(new_hotel)
+
+        return new_hotel, 200 
