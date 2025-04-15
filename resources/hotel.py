dd0406e0685b5b20cde3ac5e9bb9f847425556fc
vinicius marques <viniciusmc486@gmail.com>
2025-04-15 18:58:26 -0300

    Implement hotel search functionality with database queries and path parameter normalization

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,11 +1,86 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
+from flask import request
+import sqlite3
+
+def normalize_path_params(city = None,
+                          stars_max = 5,
+                          stars_min = 0,
+                          daily_max = 10000,
+                          daily_min = 0,
+                          limit = 50,
+                          offset = 0,
+                          **kwargs):
+    if city:
+        return {
+            "city": city,
+            "stars_min": stars_min,
+            "stars_max": stars_max,
+            "daily_min": daily_min,
+            "daily_max": daily_max,
+            "limit": limit,
+            "offset": offset
+        }
+    return {
+        "stars_min": stars_min,
+        "stars_max": stars_max,
+        "daily_min": daily_min,
+        "daily_max": daily_max,
+        "limit": limit,
+        "offset": offset
+    }
+
+
+path_params = reqparse.RequestParser()
+path_params.add_argument('city', type=str)
+path_params.add_argument('stars_max', type=float)
+path_params.add_argument('stars_mim', type=float)
+path_params.add_argument('daily_max', type=float)
+path_params.add_argument('daily_min', type=float)
+path_params.add_argument('limit', type=float)
+path_params.add_argument('offset', type=float)
 
 
 class Hoteis(Resource):
     def get(self):
-        return {"hoteis": [hotel.json() for hotel in HotelModel.query.all()]}
+        # Conexão com o banco de dados
+        conn = sqlite3.connect('instance/banco.db')
+        cursor = conn.cursor()
+
+        # dados = path_params.parse_args()
+        dados = request.args.to_dict()
+        dados_validos = {key: value for key, value in dados.items() if value is not None}
+        parametros = normalize_path_params(**dados_validos)
+
+        if parametros.get('city'):
+            query = "SELECT * FROM hoteis \
+                WHERE city = ? \
+                AND stars BETWEEN ? AND ? \
+                AND daily BETWEEN ? AND ? \
+                LIMIT ? OFFSET ?"
+            query_params = tuple([value for value in parametros.values()])
+            results = cursor.execute(query, query_params)
+        else:
+            query = "SELECT * FROM hoteis \
+                WHERE stars BETWEEN ? AND ? \
+                AND daily BETWEEN ? AND ? \
+                LIMIT ? OFFSET ?"
+            query_params = tuple([value for value in parametros.values()])
+            results = cursor.execute(query, query_params)
+
+        hoteis = []
+        for row in results:
+            hoteis.append({
+                'hotel_id': row[0],
+                'name': row[1],
+                'stars': row[2],
+                'daily': row[3],
+                'city': row[4]
+            })
+        conn.close()
+
+        return {"hoteis": hoteis}, 200
 
 
 class Hotel(Resource):
